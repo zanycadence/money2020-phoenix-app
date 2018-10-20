7b63942e45e4ecfcd3b97138e0d5763385ae66c4
Kirill Dubovitskiy <kirill2003de@gmail.com>
2018-10-20 14:19:45 -0700

    fixed naming for VisaHelper

--- money2020/lib/money2020/visa_helper.ex
+++ money2020/lib/money2020/visa_helper.ex
@@ -1,36 +1,7 @@
-defmodule H do
+defmodule Money2020.VisaHelper do
     alias Money2020.VisaApiClient
-
-    # %{ user_id: user_id,
-    #     alias_name: alias_name,
-    #     first_name: first_name,
-    #     last_name: last_name,
-    #     country: country,
-    #     issuer_name: issuer_name,
-    #     card_number: card_number,
-    #     card_type: card_type, # like Visa Classic
-    #     alias_type: alias_type
-    #     }
+    
     def create(user_id) do
-
-        # alias_type = case alias_type do
-        #     :phone -> "01"
-        #     :email -> "2"
-        # end
-
-        # currentTime = DateTime.utc_now() |> DateTime.to_string() |> String.slice(0..-9)
-        # body = %{
-        #     "guid" => user_id,
-        #     "recipientFirstName" => first_name,
-        #     "recipientLastName" => last_name,
-        #     "country" => country,
-        #     "consentDateTime" => "2018-06-02 11:20:00", #currentTime,
-        #     "recipientPrimaryAccountNumber" => card_number,
-        #     "issuerName" => issuer_name,
-        #     "cardType" => card_type,
-        #     "alias" => alias_name,
-        #     "aliasType" => alias_type
-        # }
         body = %{
             "guid" => user_id,
             "recipientFirstName" => "Jamie",
@@ -49,7 +20,7 @@ defmodule H do
             "aliasType" => "01"
         }
         case VisaApiClient.post("/visaaliasdirectory/v1/manage/createalias", (Poison.encode! body)) do
-            { :ok, response } -> response
+            { :ok, response } -> response.body
             error -> IO.inspect error
         end
     end
@@ -63,5 +34,5 @@ defmodule H do
             { :ok, response } -> response.body
             error -> IO.inspect error
         end
-    end 
+    end
 end
